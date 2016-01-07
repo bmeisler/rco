76bd46c9371ecb4912898c31f510ce2f222572c6
Bernard Meisler <bmeisler@gmail.com>
2016-01-07 12:48:43 -0800

    working on menu bar.

--- js/menu-loader.js
+++ js/menu-loader.js
@@ -15,3 +15,23 @@ $('#myTab a').click(function (e) {
   e.preventDefault();
   $(this).tab('show');
 })
+
+//change the navbar depending on the scroll position
+ $(document).scroll( function() {
+  
+   var value = $(this).scrollTop();
+   console.log(value);
+   if ( value > 100  ){
+      console.log("do something");
+      $nav.animate({height:45},"slow");
+      $('nav').css('background-color', 'red');
+      $('nav').removeClass('navbar-fixed-top');
+      $('body').css('background-color', 'red');
+      navIsBig = false;
+
+   } else if (value <= 100  ) {
+      $nav.animate({height:100},"slow");
+      $('nav').css('background-color', 'white');
+      navIsBig = true;
+  }
+ });
