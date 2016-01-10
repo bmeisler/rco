14a3678f61bcdb9ae112ad44669b6e7a4892820a
Bernard Meisler <bmeisler@gmail.com>
2016-01-09 18:41:19 -0800

    footer almost done. Need to complete email and MailChimp settings

--- js/menu-loader.js
+++ js/menu-loader.js
@@ -1,6 +1,6 @@
 $(function(){
       $("#include-rco-header").load("inc/rco-header.html"); 
-      $("#include-rco-footer").load("inc/rco-footer.html"); 
+      // $("#include-rco-footer").load("inc/rco-footer.html"); 
 });
 
 $(document).ready(function () {
@@ -11,7 +11,12 @@ $(document).ready(function () {
             $(this).addClass('active');                
         }); 
 
-        /* Smooth Scroll */
+  /* Scroll on Top */
+  $('.scrolltotop, .navbar-brand').click(function(evt){
+    $('html, body').animate({scrollTop: '0'}, 1200, 'easeInOutCubic');
+    pde(evt);
+  });
+  /* Smooth Scroll */
   jQuery('#my-nav li').bind('click',function(event){
       var anchor = jQuery(this).find('a');
       jQuery('#my-nav li').removeClass('active');
@@ -20,7 +25,15 @@ $(document).ready(function () {
           scrollTop: jQuery(anchor.attr('href')).offset().top-50
       }, 1500,'easeInOutExpo');
       event.preventDefault();
-  });           
+  });
+  /* Scroll with class */
+  jQuery('.scroll').bind('click',function(event){
+      var anchor = jQuery(this);
+    jQuery('html, body').stop().animate({
+          scrollTop: jQuery(anchor.attr('href')).offset().top-50
+      }, 1500,'easeInOutExpo');
+      event.preventDefault();
+  });         
     });
 
 $('#myTab a').click(function (e) {
