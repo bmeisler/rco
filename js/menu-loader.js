b952fc33b76c86349ad6b871e20535814d923107
Bernard Meisler <bmeisler@gmail.com>
2016-01-09 23:47:45 -0800

    menu is starting to work on scroll spy

--- js/menu-loader.js
+++ js/menu-loader.js
@@ -40,23 +40,57 @@ $('#myTab a').click(function (e) {
   e.preventDefault();
   $(this).tab('show');
 })
-
-//change the navbar depending on the scroll position
- $(document).scroll( function() {
-  
-   var value = $(this).scrollTop();
-   console.log(value);
-   if ( value > 100  ){
-      console.log("do something");
-      $nav.animate({height:45},"slow");
-      $('nav').css('background-color', 'red');
-      $('nav').removeClass('navbar-fixed-top');
-      $('body').css('background-color', 'red');
-      navIsBig = false;
-
-   } else if (value <= 100  ) {
-      $nav.animate({height:100},"slow");
-      $('nav').css('background-color', 'white');
-      navIsBig = true;
+$(document).scroll(function (){ 
+  var value = $(this).scrollTop();
+  console.log(value);
+  /* hide symbol */
+  // if ( value > 100 ){
+  //   $("#hysta-logo").addClass('hidden');
+  // } else {
+  //   $("#hysta-logo").removeClass('hidden');
+  // }
+  //  Show "To Top" button 
+  // if ( value > 100 ){
+  //   $(".scrolltotop").addClass('show-to-top');
+  // } else {
+  //   $(".scrolltotop").removeClass('show-to-top');
+  // }
+  /* Minified bar */
+  if ( value > 100 ){
+    console.log("make it static")
+    $("#top-menu").addClass('navbar-fixed-top');
+    $("#top-menu").removeClass('navbar-static-top');
+    $(".navbar-brand").removeClass('hidden');
+    
+  } else {
+    $("#top-menu").removeClass('navbar-fixed-top');
+    $("#top-menu").addClass('navbar-static-top');
+    $(".navbar-brand").addClass('hidden');
   }
- });
+  /* User Loogin */
+  // if ( value > 100 ){
+  //   $("#user").addClass('less-mt');
+  // } else {
+  //   $("#user").removeClass('less-mt');
+  // }
+});
+//change the navbar depending on the scroll position
+ // $(document).scroll( function() {
+  
+ //   var value = $(this).scrollTop();
+ //   console.log(value);
+ //   if ( value > 100  ){
+ //      console.log("do something");
+ //      $nav.animate({height:45},"slow");
+ //      $('nav').css('background-color', 'red');
+ //      $('#top-menu').removeClass('navbar-fixed-static');
+ //      $('#top-menu').addClass('navbar-fixed-top');
+ //      $('body').css('background-color', 'red');
+ //      navIsBig = false;
+
+ //   } else if (value <= 100  ) {
+ //      $nav.animate({height:100},"slow");
+ //      $('nav').css('background-color', 'white');
+ //      navIsBig = true;
+ //  }
+ // });
