24376d508771b78cc00422caed706c1b7eabc97a
Bernard Meisler <bmeisler@gmail.com>
2016-01-14 16:38:15 -0800

    top and bottom menus looking good. Signup popup on top working. Bottom menu signup looks good at all sizes.

--- inc/rco-footer.php
+++ inc/rco-footer.php
@@ -1,7 +1,7 @@
 <section id="footer">
   <div class="container">
       <div class="row">
-        <div class="col-sm-4 footer-border"><!-- 1st column -->
+        <div class="col-sm-4 footer-border first-col"><!-- 1st column -->
           <h3 class="mb40">Russian</br>
           Chamber</br>
           Orchestra</h3>
@@ -20,6 +20,8 @@
           Copyright &#169;&nbsp;<script>document.write(new Date().getFullYear())</script><noscript>2015</noscript> Russian Chamber Orchestra. </br>All Rights Reserved.
         </div>
         </div>
+
+
         <div class="col-sm-4 footer-border"><!-- 2nd column -->
           <h3 class="mb40">Contact</h3>
           For general Inquiry
@@ -43,44 +45,44 @@
         <h3>Join our mailing list</h3>
 
        <!-- Begin MailChimp Signup Form -->
-<div id="mc_embed_signup">
+        <div id="mc_embed_signup">
   <form action="//sensitiveskinmagazine.us2.list-manage.com/subscribe/post?u=df2b62f883c43ba3749ff7368&amp;id=c53aaba024" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
       <div class="mc-field-group">
-      <input type="email" value="" name="EMAIL" placeholder="Email Address (required)" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" class="required form-control input-lg" id="mce-EMAIL">
+        <input type="email" value="" name="EMAIL" placeholder="Email Address (required)" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" class="required form-control input-lg" id="mce-EMAIL">
       </div>
       <div class="row form-group">
-          <div class="col-xs-6 input-lg-left">
-            <div class="mc-field-group">
+        <div class="col-xs-6 input-lg-left">
+          <div class="mc-field-group">
             
             <input type="text" value="" name="FNAME" placeholder="First Name" onfocus="this.placeholder=''" onblur="this.placeholder='First Name'" class="form-control input-lg" id="mce-FNAME">
           </div>
-      </div>
-      <div class="col-xs-6 input-lg-right">
-        <div class="mc-field-group">
-          <input type="text" value="" name="LNAME" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" class="form-control input-lg" id="mce-LNAME">
+        </div>
+        <div class="col-xs-6 input-lg-right">
+          <div class="mc-field-group">
+            <input type="text" value="" name="LNAME" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" class="form-control input-lg" id="mce-LNAME">
           </div>
-      </div>
-      <div id="mce-responses" class="clear">
-        <div class="response" id="mce-error-response" style="display:none">
         </div>
-        <div class="response" id="mce-success-response" style="display:none">
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none">
+          </div>
+          <div class="response" id="mce-success-response" style="display:none">
+          </div>
+        </div>
+        
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input type="text" name="b_df2b62f883c43ba3749ff7368_c53aaba024" tabindex="-1" value="">
+        </div>
+        <div class="clear">
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">
         </div>
       </div>
-      
-      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-      <div style="position: absolute; left: -5000px;" aria-hidden="true">
-        <input type="text" name="b_df2b62f883c43ba3749ff7368_c53aaba024" tabindex="-1" value="">
-      </div>
-      <div class="clear">
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">
-      </div>
-        </div>
     </form>
   </div>
-
-<!--End mc_embed_signup-->
-        </div>
+  
+  <!--End mc_embed_signup-->
+</div>
       </div>
     </div>
     </section>
--- inc/rco-header.php
+++ inc/rco-header.php
@@ -19,11 +19,11 @@
 <body data-spy="scroll" data-target="#my-nav" id="cbp-so-scroller">
 
 <div class="rco-header">
-    <div class="row vertical-align-top">
+    <div class="row vertical-align-middle">
         <div class="col-xs-6"> 
           <!-- <h1 class="h1-top">Russian Chamber Orchestra</h1> -->
-          <div class="russian-header">Русский камерный оркестр</div>
-        </div>
+<!--           <div class="russian-header">Русский камерный оркестр</div>
+ -->        </div>
          <div class="col-xs-6">
          <div class="music-director"><strong>Alexander Vereshagin</strong> – Music Director &amp; Conductor</div>
         </div>
@@ -44,24 +44,68 @@
       </button>
        <div class="mobile-icons visible-xs-block">
         <a data-toggle="collapse" data-target="#search-bar" class="circl"><i class="fa fa-search"></i></a>
-        <a data-toggle="collapse" data-target="#user" class="circl"><i class="fa fa-user"></i></a>
+        <a data-toggle="collapse" data-target="#signup-bar" class="circl"><i class="fa fa-user"></i></a>
       </div>
       <a class="navbar-brand" href="index.php">Russian Chamber Orchestra</a>
       <!-- Search -->
-<div id="search-bar" class="collapse">
-  <div class="container">
-    <div class="row">
-      <div class="search">
-        <form method="post" action="search.htm">
-          <div class="form-group">
-            <input type="text" class="form-control input-lg" name="" onfocus="this.placeholder=''" onblur="this.placeholder='Type in search here'" placeholder="Type in search here">
+      <div id="search-bar" class="collapse">
+        <div class="container">
+          <div class="row">
+            <div class="search">
+              <form method="post" action="search.htm">
+                <div class="form-group">
+                  <input type="text" class="form-control input-lg" name="" onfocus="this.placeholder=''" onblur="this.placeholder='Type in search here'" placeholder="Type in search here">
+                </div>
+              </form>
+              <a class="search-closer" data-toggle="collapse" data-target="#search-bar"><i class="fa fa-times"></i></a>
+            </div>
           </div>
-        </form>
-        <a class="search-closer" data-toggle="collapse" data-target="#search-bar"><i class="fa fa-times"></i></a>
+        </div>
       </div>
-    </div>
-  </div>
+
+      <div id="signup-bar" class="collapse">
+         <h4>Join our mailing list</h4>
+           <div id="mc_embed_floating_signup">
+          <form action="//sensitiveskinmagazine.us2.list-manage.com/subscribe/post?u=df2b62f883c43ba3749ff7368&amp;id=c53aaba024" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <div class="mc-field-group">
+                <input type="email" value="" name="EMAIL" placeholder="Email Address (required)" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" class="required form-control input-lg" id="mce-EMAIL">
+              </div>
+              <div class="row form-group">
+                <div class="col-xs-6 input-lg-left">
+                  <div class="mc-field-group">
+                    
+                    <input type="text" value="" name="FNAME" placeholder="First Name" onfocus="this.placeholder=''" onblur="this.placeholder='First Name'" class="form-control input-lg" id="mce-FNAME">
+                  </div>
+                </div>
+                <div class="col-xs-6 input-lg-right">
+                  <div class="mc-field-group">
+                    <input type="text" value="" name="LNAME" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" class="form-control input-lg" id="mce-LNAME">
+                  </div>
+                </div>
+                <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none">
+                  </div>
+                  <div class="response" id="mce-success-response" style="display:none">
+                  </div>
+                </div>
+                
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                  <input type="text" name="b_df2b62f883c43ba3749ff7368_c53aaba024" tabindex="-1" value="">
+                </div>
+                <div class="clear">
+                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+  
+  <!--End mc_embed_signup-->
 </div>
+
+
     </div>
       
     <div class="collapse navbar-collapse" id="my-nav" role="navigation">
@@ -71,7 +115,7 @@
         <li><a href="support-and-volunteer.php" >Volunteer</a></li>
         <li><a href="musicians.php" >Music & Soloists</a></li>
         <li><a href="about.php" >About</a></li>
-        <li class="hidden-xs"><a data-toggle="collapse" data-target="#user"><i class="circl crl-left"><i class="fa fa-user"></i></i></a></li>
+        <li class="hidden-xs"><a data-toggle="collapse" data-target="#signup-bar"><i class="circl crl-left"><i class="fa fa-user"></i></i></a></li>
         <li class="hidden-xs"><a data-toggle="collapse" data-target="#search-bar"><span class="fa fa-search" ></span></a></span></a></li>
 
       </ul>
