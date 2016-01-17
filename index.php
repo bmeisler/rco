fa9eac5cd1077db755d97f3af8d23f8a98d86df9
Bernard Meisler <bmeisler@gmail.com>
2016-01-17 14:48:09 -0800

    fixed seo on pages. Adjusted front page image

--- index.php
+++ index.php
@@ -15,9 +15,12 @@
 
 <div class="container">
      <div class="row">
-     <div class="col-md-6"> 
+     <div class="col-sm-5" id="next-concert"> 
       <h2>Next Concert <br><small><a class="btn btn-primary" href="http://www.brownpapertickets.com/event/1742440" target="_blank">Buy tickets online</a></small></h2>
      
+      </div>
+      <div class="col-sm-7"> 
+      <img src="images/rco.jpg" class="img-responsive" alt="Russian Chamber Orchestra">
     </div>
      </div>
     
@@ -26,100 +29,17 @@
 
 
 
-  <div class="row">
-     <div class="col-md-8" id="next-concert"> 
-      <!-- <h4 class="rco-date">October 25, 2015</h4>
-
-        <address><strong>Mt. Tamalpais United Methodist Church</strong><br>
-        410 Sycamore Avenue, Mill Valley, CA 94941<br>
-        Concert begins at 4:00 pm</address>
-
-        <p class="rco-pieces"><em>J. S. Bach</em> - Violin Concerto in A minor, BWV 1041<br>
-        <em>W. A. Mozart</em> - Piano Concerto #6, K. 238<br>
-        <em>J. Brahms</em> - Liebeslieder Waltzes, Op. 52 & 65
-
-        <h2 class="rco-players"><a href="musicians.html#Alena Tsoi"><em>Alena Tsoi,</em></a> violin</h2>
-        <h2 class="rco-players"><a href="musicians.html#Marilyn Thompson"><em>Marilyn Thompson,</em></a> piano</h2>
-      </div>
-      <div class="col-md-4"> 
-      </div>
-  </div>
-   <div class="row">
-     <div class="col-md-8"> 
-      <h4 class="rco-date">December 6, 2015</h4>
-
-        <address><strong>Mt. Tamalpais United Methodist Church</strong><br>
-        410 Sycamore Avenue, Mill Valley, CA 94941<br>
-        Concert begins at 4:00 pm</address>
-
-        <p class="rco-pieces"><em>W. A. Mozart</em> - Serenade #6, K. 239<br>
-        <em>P. Sarasate</em> - Gypsy Airs, Op. 20
-<br>
-        <em>F. Mendelssohn</em> - Piano Concerto #1, Op. 25
-
-        <h2 class="rco-players"><a href="musicians.html#Alena Tsoi"><em>Alena Tsoi,</em></a> violin</h2>
-        <h2 class="rco-players"><a href="musicians.html#Irina Behrendt"><em>Irina Behrendt,</em></a> piano</h2>
+ <!--  <div class="row">
+        
+     <div class="col-sm-4" id="next-concert"> 
+  
     </div>
-  </div>
-
-   <div class="row">
-     <div class="col-md-8"> 
-      <h4 class="rco-date">January 24, 2016</h4>
-
-        <address><strong>Mt. Tamalpais United Methodist Church</strong><br>
-        410 Sycamore Avenue, Mill Valley, CA 94941<br>
-        Concert begins at 4:00 pm</address>
-
-         <p class="rco-pieces"><em>A. Vivaldi</em> - Violin Concerto “Winter”<br>
-          <em>P. Tchaikovsky </em>- Pas de deux from “Swan Lake”<br>
-<em>W. A. Mozart</em> - Piano Concerto #12, K. 414<br>
-
-        <h2 class="rco-players"><a href="musicians.html#Mariya Borozina"><em>Mariya Borozina,</em></a> violin</h2>
-        <h2 class="rco-players"><a href="musicians.html#Victoria Ehrlich"><em>Victoria Ehrlich,</em></a> cello</h2>
-        <h2 class="rco-players"><a href="musicians.html#Marilyn Thompson"><em>Marilyn Thompson,</em></a> piano</h2>
-    </div>
-  </div>
-
-   <div class="row">
-     <div class="col-md-8"> 
-      <h4 class="rco-date">March 6, 2016</h4>
-
-        <address><strong>Mt. Tamalpais United Methodist Church</strong><br>
-        410 Sycamore Avenue, Mill Valley, CA 94941<br>
-        Concert begins at 4:00 pm</address>
-
-        <p class="rco-pieces"><em>A. Vivaldi</em> - Violin Concerto “Winter”<br>
-<em>P. Tchaikovsky</em> - Pas de deux from “Swan Lake”</br>
-<em>W. A. Mozart</em> - Piano Concerto #12, K. 414
-
-        <h2 class="rco-players"><a href="musicians.html#Mariya Borozina"><em>Mariya Borozina,</em></a> violin</h2>
-        <h2 class="rco-players"><a href="musicians.html#Victoria Ehrlich"><em>Victoria Ehrlich,</em></a> cello</h2>
-        <h2 class="rco-players"><a href="musicians.html#Marilyn Thompson"><em>Marilyn Thompson,</em></a> piano</h2>
-      </div>
-  </div>
-  <div class="row">
-     <div class="col-md-8"> 
-      <h4 class="rco-date">May 8, 2016</h4>
-
-        <address><strong>Mt. Tamalpais United Methodist Church</strong><br>
-        410 Sycamore Avenue, Mill Valley, CA 94941<br>
-        Concert begins at 4:00 pm</address>
-
-        <p class="rco-pieces"><em>A. Vivaldi </em>- Violin Concerto “Summer”
-<em>P. Tchaikovsky</em> - Andante cantabile, Op. 11
-<em>F. J. Haydn</em> - Piano Concerto in D major
-
-        <h2 class="rco-players"><a href="musicians.html#Elina Lev"><em>Elina Lev,</em></a> violin</h2>
-        <h2 class="rco-players"><a href="musicians.html#Irina Behrendt"><em>Irina Behrendt,</em></a> piano</h2>
-</div> -->
-  </div>
-  </div>
-       <div class="row">
-     <div class="col-md-12"> 
+     <div class="col-sm-8"> 
       <img src="images/rco.jpg" class="img-responsive" alt="Russian Chamber Orchestra">
     </div>
+
     
-  </div>
+  </div> -->
 </div>
 
 <?php include('inc/rco-footer.php'); ?>
--- support-and-volunteer.php
+++ support-and-volunteer.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Russian Chamber Orchestra - Support</title>
+    <title>Russian Chamber Orchestra - Support and Volunteer</title>
 
 
 
